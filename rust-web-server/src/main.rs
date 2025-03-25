50227a9605c8b08483d4d924e8e1e80495047944
Bill Figliolini <BillFigliolini@gmail.com>
2025-03-25 19:59:54 +0000

    Removed unecessary functions for cleaner control flow and compiler warning compliance

--- rust-web-server/src/main.rs
+++ rust-web-server/src/main.rs
@@ -23,35 +23,14 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
     println!("Request: {:?}", http_request);
 
+    let request_line = &http_request[0];
 
-    if http_request.len() == 0 {
-        return;
-    }
-    if http_request[0].contains("favicon.ico") {
-        return;
-    }
-    if http_request[0].contains("GET / HTTP/1.1") {
-        get_home_page(stream);
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" { 
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        get_404_page(stream);
-    }
-}
-
-fn get_home_page(mut stream: TcpStream) {
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line, length, contents
-    );
-
-    stream.write(response.as_bytes()).unwrap();
-}
-
-fn get_404_page(mut stream: TcpStream){
-    let status_line = "HTTP/1.1 404 NOT FOUND";
-    let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
