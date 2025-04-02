b7f618cc2999666e626cdb51d9508e97068f6d6e
Bill Figliolini <BillFigliolini@gmail.com>
2025-04-01 22:28:15 -0400

    Rust-fmt linter run

--- rust-web-server/src/lib.rs
+++ rust-web-server/src/lib.rs
@@ -4,12 +4,11 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::TcpStream,
-
 };
 
 pub fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request : Vec<_> = buf_reader
+    let http_request: Vec<_> = buf_reader
         .lines()
         .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
--- rust-web-server/src/multithreading/threadpool.rs
+++ rust-web-server/src/multithreading/threadpool.rs
@@ -1,22 +1,22 @@
-use::std::{
+use ::std::{
     sync::{mpsc, Arc, Mutex},
     thread,
 };
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
-pub struct ThreadPool{
+pub struct ThreadPool {
     workers: Vec<Worker>,
     dispatcher: Option<mpsc::Sender<Job>>,
 }
 
-impl ThreadPool{
+impl ThreadPool {
     /// Create a new ThreadPool.
-    /// 
+    ///
     /// The size is the number of threads in the pool.
-    /// 
+    ///
     /// # Panics
-    /// 
+    ///
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
@@ -30,7 +30,10 @@ impl ThreadPool{
             // create some threads and store them in the vector
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, dispatcher: Some(dispatcher) }
+        ThreadPool {
+            workers,
+            dispatcher: Some(dispatcher),
+        }
     }
     pub fn execute<F>(&self, f: F)
     where
@@ -42,7 +45,7 @@ impl ThreadPool{
     }
 }
 
-impl Drop for ThreadPool{
+impl Drop for ThreadPool {
     fn drop(&mut self) {
         drop(self.dispatcher.take());
 
@@ -62,19 +65,22 @@ struct Worker {
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let message = receiver.lock().expect("Worker attempted to acquire Mutex from Panicked thread, shutting down.").recv();
+            let message = receiver
+                .lock()
+                .expect("Worker attempted to acquire Mutex from Panicked thread, shutting down.")
+                .recv();
 
             match message {
                 Ok(job) => {
                     println!("Worker {} got a job; executing.", id);
                     job();
-                },
+                }
                 Err(_) => {
                     println!("Worker {} is shutting down.", id);
                     break;
                 }
             }
         });
-        Worker { id, thread}
+        Worker { id, thread }
     }
 }
